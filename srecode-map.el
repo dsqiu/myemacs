e8d6e82f0ca76cd456e70558e0e3e63f452da92a
dsqiu <dsqiu@ingenic.cn>
2014-10-25 23:03:48 +0800

    delete some useless files.

--- srecode-map.el
+++
@@ -1,6 +0,0 @@
-;; Object SRecode Map
-;; EIEIO PERSISTENT OBJECT
-(srecode-map "SRecode Map"
-  :file "srecode-map.el"
-  :files '(("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/wisent.srt" . wisent-grammar-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/texi.srt" . texinfo-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/template.srt" . srecode-template-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/make.srt" . makefile-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/java.srt" . java-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/el.srt" . emacs-lisp-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/default.srt" . default) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/cpp.srt" . c++-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/c.srt" . c-mode))
-  :apps '((tests ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/test.srt" . srecode-template-mode)) (getset ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/getset-cpp.srt" . c++-mode)) (ede ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/ede-make.srt" . makefile-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/ede-autoconf.srt" . autoconf-mode)) (document ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/doc-java.srt" . java-mode) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/doc-default.srt" . default) ("/home/dsqiu/tools/emacs/emacs.git/etc/srecode/doc-cpp.srt" . c++-mode))))
